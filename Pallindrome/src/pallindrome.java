66ab54010150f652be3c006d9ea61671a1d3299b
Madhu <Madhu@LAPTOP-V1O0VD67>
2022-05-14 13:03:23 +0530

    Pallindrome

---
+++ Pallindrome/src/pallindrome.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class pallindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter the number ");
+	    int num = sc.nextInt();
+	    int temp = num;
+	    int revNum = 0;
+	    while (num!=0) {
+	    	int digit = num% 10;
+	    	revNum = (revNum * 10) + digit;
+	    	num/=10;
+	    	
+	    }
+	    
+	    if(revNum == temp) {
+	    	System.out.println(temp +" is a pallindrome");
+	    }
+	    else {
+	    	System.out.println(temp +" is not ca pallindrome");
+	    }
+	}
+
+}
